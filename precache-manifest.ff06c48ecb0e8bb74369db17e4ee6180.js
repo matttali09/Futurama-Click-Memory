da6db112433691728eae0c11b54ffdda1f637b6d
matttali09 <matttaliancich09@gmail.com>
2019-01-18 11:17:22 -0500

    Updates

---
+++ precache-manifest.ff06c48ecb0e8bb74369db17e4ee6180.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f87b1ee6f1eab1b1774",
+    "url": "/Futurama-Click-Memory/static/js/runtime~main.7f87b1ee.js"
+  },
+  {
+    "revision": "df17eafff44dd48ede5e",
+    "url": "/Futurama-Click-Memory/static/js/main.df17eaff.chunk.js"
+  },
+  {
+    "revision": "f5bc1ae19067e9c6c11e",
+    "url": "/Futurama-Click-Memory/static/js/1.f5bc1ae1.chunk.js"
+  },
+  {
+    "revision": "df17eafff44dd48ede5e",
+    "url": "/Futurama-Click-Memory/static/css/main.723fdb18.chunk.css"
+  },
+  {
+    "revision": "577d675c75e4613e71f222e3d0428ca3",
+    "url": "/Futurama-Click-Memory/index.html"
+  }
+];
